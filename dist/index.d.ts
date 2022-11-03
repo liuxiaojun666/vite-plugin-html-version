b3638810dbf6d833e0bf8c683ce1cdbb30f386d1
liuxiaojun <lxj_web@163.com>
2022-11-03 11:58:10 +0800

    参数选填

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -2,6 +2,6 @@ import { Plugin } from 'vite';
 interface Options {
     version?: string | (() => string);
 }
-declare const _default: ({ version }: Options) => Plugin;
+declare const _default: ({ version }?: Options) => Plugin;
 export default _default;
 //# sourceMappingURL=index.d.ts.map
